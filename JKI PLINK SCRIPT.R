48ee31233fdfa9395cbae5443e3c8f114ed3fd5a
Aaron Hewson <aaronjhewson@gmail.com>
2025-08-03 19:11:44 +1200

    loading data

--- JKI PLINK SCRIPT.R
+++ JKI PLINK SCRIPT.R
@@ -11,5 +11,26 @@ library(tidyr)
 #Set wd
 setwd("C:/Users/curly/Desktop/Apple Genotyping/Methods/JKI PLINK Duplicate Identification/Inputs")
 
-#Load JKI data
+#Load JKI data and transpose
 JKI <- read.delim("JKI_Samples_Fixed.txt", header = FALSE, stringsAsFactors = FALSE)
+JKI_t <- as.data.frame(t(JKI))
+
+#Add empty columns for PLINK formatting
+JKI_t <- add_column(JKI_t, Fa = 0, Mo = 0, Se = 0, Ph = 0, .before = "V2" )
+JKI_t <- add_column(JKI_t, Fid = 0, .before = "V1" )
+
+#Remove header row 
+JKI_t = JKI_t[-1, ]
+
+#Remove row and column names
+colnames(JKI_t) <- NULL
+rownames(JKI_t) <- NULL
+
+#Load in PLINK .ped
+ped <- read.csv("JD_PFR_PLINK.ped", header = FALSE,sep = "\t")
+
+#Bind JKI data onto PLINK .ped
+names(JKI_t) <- names(ped)
+combined_ped <- bind_rows(ped, JKI_t)
+
+
